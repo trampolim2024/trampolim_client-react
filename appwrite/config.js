29d3cdcb3c44d86594ac82af1b6cd0d28b9f5f60
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-08-28 20:15:23 -0300

    feat: added appwrite Client configuration

---
+++ appwrite/config.js
@@ -0,0 +1,7 @@
+import { Client } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
