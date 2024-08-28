a8131df18c39d2fd74f37959f382a6c9217cd22e
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-08-28 19:57:11 -0300

    feat: initial structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className='bg-orange-400 w-[80vw] h-screen rounded-2xl flex items-center justify-center'>
+        <h1 className='font-bold text-5xl text-blue-600'>Programa Trampolim</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
