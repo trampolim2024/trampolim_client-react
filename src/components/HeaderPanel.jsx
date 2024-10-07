0372164b662d276c5f83d7157a4c9a4399e573af
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-10-07 20:11:39 -0300

    feat: added new component: HeaderPanel

---
+++ src/components/HeaderPanel.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const HeaderPanel = () => {
+    return (
+        <div className='w-full h-[20vh] p-3 flex items-center justify-around shadow-xl background-white'>
+
+            <Link to="/">
+                <img src="logos/trampolim-header.svg" alt="Logo do Programa Trampolim" className='' />
+            </Link>
+
+            <div className='flex items-center w-[50%] justify-evenly '>
+                <Link to="/painel" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
+                    Painel
+                </Link>
+
+                <a href="#" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
+                    Eventos
+                </a>
+
+                <Link to="/avaliador" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
+                    Certificados
+                </Link>
+
+                <a href="#" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
+                    Diagnósticos
+                </a>
+
+              
+            </div>
+        </div>
+    )
+}
+
+export default HeaderPanel
