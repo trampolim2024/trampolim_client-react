5c4c326f0707c82ff591e0462aa95322225b786d
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-06 14:07:50 -0300

    fix: remove authcontext layouts and appwrite

--- appwrite/config.js
+++
@@ -1,6 +0,0 @@
-import { Client } from 'appwrite';
-
-const client = new Client();
-client.setProject('672b6d9e0038ae323e69');
-
-export default Client;
--- src/utils/PrivateRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate} from "react-router-dom";
-
-const PrivateRoutes = () => {
-    const user = false;
-    return user ? <Outlet/> : <Navigate to="/login"/>
-}
-
-export default PrivateRoutes;
