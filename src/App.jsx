74e19e0a865f0942d6859c02e526a919935b6c15
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-06 10:46:32 -0300

    feat: new route wrapper to manager the auththentication system

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,24 @@ import AppraiserSignup from "./routes/AppraiserSignup";
 import EnterprisingSignup from "./routes/EnterprisingSIgnup";
 import Login from "./routes/Login";
 import Panel from "./pages/Panel";
+import PrivateRoutes from "./utils/PrivateRoutes"
 
 function App() {
 
   return (
     <Router>
       <Routes>
+
         <Route path="/" element={<LandingPage />} />
-        <Route path="/avaliador" element={<Appraiser/>}/>
-        <Route path="/inscricao-avaliador" element={<AppraiserSignup/>}/>
-        <Route path="/inscricao-empreendedor" element={<EnterprisingSignup/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/painel" element={<Panel/>}/>
+        <Route path="/avaliador" element={<Appraiser />} />
+        <Route path="/inscricao-avaliador" element={<AppraiserSignup />} />
+        <Route path="/inscricao-empreendedor" element={<EnterprisingSignup />} />
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<PrivateRoutes />}>
+          <Route path="/painel" element={<Panel />} />
+        </Route>
+        
       </Routes>
     </Router>
   )
