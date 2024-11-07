8d15e0ff819b88e64a12fcaec3208bc25070ecaf
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-07 15:55:27 -0300

    feat: middleware function

--- src/pages/Panel.jsx
+++ src/pages/Panel.jsx
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "../components/Footer";
 import Filter from "../components/FIlter";
 import Searchbar from "../components/Searchbar";
 import HeaderPanel from "../components/HeaderPanel";
+import { useNavigate } from "react-router-dom";
 
 const Panel = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        if (!token) {
+          console.log("Token não encontrado. Redirecionando para login.");
+          navigate("/login");
+          return;
+        }
+
+        const response = await fetch("http://localhost:3000/usuarios/painel", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.ok) {
+          console.log("Autenticação bem-sucedida. Usuário autorizado a acessar o painel.");
+        } else if (response.status === 401 || response.status === 403) {
+          console.log("Token inválido ou expirado. Redirecionando para login.");
+          navigate("/login");
+        } else {
+          console.log("Erro desconhecido:", response.statusText);
+        }
+      } catch (error) {
+        console.error("Erro na autenticação:", error);
+        navigate("/login");
+      }
+    };
+
+    checkAuthentication();
+  }, [navigate]);
+
   return (
     <main className="h-auto">
       <HeaderPanel />
