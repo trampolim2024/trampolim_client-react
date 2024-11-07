bbe4c5e235d7466c68ebbe668fb8b58614c32356
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-07 15:55:17 -0300

    feat: logout button

--- src/components/HeaderPanel.jsx
+++ src/components/HeaderPanel.jsx
@@ -1,36 +1,54 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import User from './User'
 
 const HeaderPanel = () => {
-    return (
-        <div className='w-full h-[20vh] p-3 flex items-center justify-around shadow-xl background-white'>
+  const navigate = useNavigate(); // Hook do React Router para navegação programática
 
-            <Link to="/">
-                <img src="logos/trampolim-header.svg" alt="Logo do Programa Trampolim" className='' />
-            </Link>
+  // Função para tratar o logout
+  const handleLogout = () => {
+    // Remove o token de autenticação do localStorage
+    localStorage.removeItem('token');
 
-            <div className='flex items-center w-[50%] justify-evenly '>
-                <Link to="/painel" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
-                    Painel
-                </Link>
+    // Redireciona o usuário para a página inicial (ou página de login)
+    navigate('/');
+  };
 
-                <a href="#" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
-                    Eventos
-                </a>
+  return (
+    <div className='w-full h-[20vh] p-3 flex items-center justify-around shadow-xl background-white'>
+      <Link to="/">
+        <img src="logos/trampolim-header.svg" alt="Logo do Programa Trampolim" className='' />
+      </Link>
 
-                <Link to="#" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
-                    Certificados
-                </Link>
+      <div className='flex items-center w-[50%] justify-evenly '>
+        <Link to="/painel" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
+          Painel
+        </Link>
 
-                <a href="#" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
-                    Diagnósticos
-                </a>
+        <a href="#" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
+          Eventos
+        </a>
 
-                <User/>
-            </div>
+        <Link to="#" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
+          Certificados
+        </Link>
+
+        <a href="#" className='font-semibold text-blue text-xl hover:text-orange transition-all duration-300'>
+          Diagnósticos
+        </a>
+
+        <div className='flex items-center justify-center gap-3 flex-col'>
+          <User />
+          {/* Botão de Sair, chamando a função handleLogout */}
+          <span 
+            onClick={handleLogout} 
+            className='text-orange font-bold text-base cursor-pointer'>
+            Sair
+          </span>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default HeaderPanel
+export default HeaderPanel;
