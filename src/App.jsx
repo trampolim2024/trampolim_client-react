50bb678ba0ef6d1ed5c99cd123f4c6d0183412a6
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-11-07 15:55:09 -0300

    feat: added toastContainer

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,30 @@ import AppraiserSignup from "./routes/AppraiserSignup";
 import EnterprisingSignup from "./routes/EnterprisingSIgnup";
 import Login from "./routes/Login";
 import Panel from "./pages/Panel";
+import ProtectedRoutes from "../utils/ProtectedRoutes";
+import { ToastContainer } from "react-toastify";
 
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
+    <>
+      <ToastContainer />
+      <Router>
+        <Routes>
 
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/avaliador" element={<Appraiser />} />
-        <Route path="/inscricao-avaliador" element={<AppraiserSignup />} />
-        <Route path="/inscricao-empreendedor" element={<EnterprisingSignup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/painel" element={<Panel />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/avaliador" element={<Appraiser />} />
+          <Route path="/inscricao-avaliador" element={<AppraiserSignup />} />
+          <Route path="/inscricao-empreendedor" element={<EnterprisingSignup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/painel" element={<ProtectedRoutes><Panel /></ProtectedRoutes>} />
 
 
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </>
+
   )
 }
 
