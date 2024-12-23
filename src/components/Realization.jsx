137eccf56dedb0ac197b53c44ba006c4d7b24e76
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-12-23 08:59:06 -0300

    feat: added the logos

--- src/components/Realization.jsx
+++ src/components/Realization.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { FaFileAlt, FaUser } from 'react-icons/fa'
+import EspacoColinas from "/logos/espacocolinas-logo.png"
+import SebraePe from "/logos/sebraepe-logo.png"
+import SeteColinas from "/logos/setecolinas-logo.png"
+import Softex from "/logos/softex-logo.png"
+import Upe from "/logos/upe-logo.png"
+
 
 const Realization = () => {
     return (
         <section className='h-auto flex flex-col gap-10 mt-20 mb-20'>
-
 
             <div className='w-full flex items-center justify-around'>
                 <div className='background-purple w-full h-[30vh] p-4 flex items-center justify-center shadow-lg flex-col'>
@@ -14,25 +19,27 @@ const Realization = () => {
 
             </div>
 
-
-
             <h1 className='ml-[5%] text-4xl text-blue uppercase font-bold'>Realização</h1>
 
             <div className='flex items-center w-full justify-around'>
                 <div className='w-[300px] h-[300px] rounded-full background-white border-2 border-[#F79B4B] flex items-center justify-center'>
-                    <FaUser className='text-blue' size={100} />
+                    <img src={EspacoColinas} alt="" />
                 </div>
 
                 <div className='w-[300px] h-[300px] rounded-full background-white border-2 border-[#F79B4B] flex items-center justify-center'>
-                    <FaUser className='text-blue' size={100} />
+                    <img src={SebraePe} alt="" />
                 </div>
 
                 <div className='w-[300px] h-[300px] rounded-full background-white border-2 border-[#F79B4B] flex items-center justify-center'>
-                    <FaUser className='text-blue' size={100} />
+                    <img src={SeteColinas} alt="" />
                 </div>
 
                 <div className='w-[300px] h-[300px] rounded-full background-white border-2 border-[#F79B4B] flex items-center justify-center'>
-                    <FaUser className='text-blue' size={100} />
+                    <img src={Softex} alt="" />
+                </div>
+
+                <div className='w-[300px] h-[300px] rounded-full background-white border-2 border-[#F79B4B] flex items-center justify-center'>
+                    <img src={Upe} alt="" className="w-[200px]" />
                 </div>
             </div>
         </section>
