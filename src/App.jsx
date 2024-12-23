db22a9dbdced1b58effdf55c8483edf95a4ecd30
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2024-12-23 09:03:51 -0300

    feat: added new routes to the App

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,20 @@ import Appraiser from "./pages/Appraiser";
 import AppraiserSignup from "./routes/AppraiserSignup";
 import EnterprisingSignup from "./routes/EnterprisingSIgnup";
 import Login from "./routes/Login";
-import Panel from "./pages/Panel";
 import ProtectedRoutes from "../utils/ProtectedRoutes";
 import { ToastContainer } from "react-toastify";
 import Enterprising from "./pages/Enterprising";
+import AppraiserPanel from "./pages/AppraiserPanel";
+import EnterprisingPanel from "./pages/EnterprisingPanel";
+import AdmPanel from "./pages/AdmPanel";
+import CreateAnnouncement from "./pages/CreateAnnouncement";
+import Projects from "./pages/Projects";
+import Announcements from "./pages/Announcements";
+import SendProject from "./pages/SendProject";
+import AppraiserProfile from "./pages/AppraiserProfile";
+import EnterprisingProfile from "./pages/EnterprisingProfile";
+import AnnoucementDetails from "./pages/AnnoucementDetails";
+import ProjectDetails from "./pages/ProjectDetails";
 
 
 function App() {
@@ -23,8 +33,18 @@ function App() {
           <Route path="/empreendedor" element={<Enterprising />} />
           <Route path="/inscricao-avaliador" element={<AppraiserSignup />} />
           <Route path="/inscricao-empreendedor" element={<EnterprisingSignup />} />
+          <Route path="/criar-edital" element={<CreateAnnouncement />} />
+          <Route path="/perfil-avaliador" element={<AppraiserProfile />} />
+          <Route path="/perfil-empreendedor" element={<EnterprisingProfile />} />
+          <Route path="/detalhes-edital" element={<AnnoucementDetails />} />
+          <Route path="/detalhes-projeto" element={<ProjectDetails />} />
+          <Route path="/projetos" element={<Projects />} />
+          <Route path="/editais" element={<Announcements />} />
+          <Route path="/enviar-projeto" element={<SendProject />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/painel" element={<ProtectedRoutes><Panel /></ProtectedRoutes>} />
+          <Route path="/painel-empreendedor" element={<ProtectedRoutes><EnterprisingPanel /></ProtectedRoutes>} />
+          <Route path="/painel-avaliador" element={<ProtectedRoutes><AppraiserPanel /></ProtectedRoutes>} />
+          <Route path="/painel-adm" element={<AdmPanel />} />
 
 
         </Routes>
